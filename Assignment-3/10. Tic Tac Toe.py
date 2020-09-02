8c6f58091dc6f4ce7cdaf6bf2572dfe5ea0b8034
Mridul Rastogi <64559497+MridulRastogi@users.noreply.github.com>
2020-09-03 00:56:50 +0530

    Add files via upload

---
+++ Assignment-3/10. Tic Tac Toe.py
@@ -0,0 +1,109 @@
+import winsound as ws
+def out(a):
+    print("_______________")
+    print("|",a[0][0],"|",a[0][1],"|",a[0][2],"|")
+    print("--------------")
+    print("|",a[1][0],"|",a[1][1],"|",a[1][2],"|")
+    print("--------------")
+    print("|",a[2][0],"|",a[2][1],"|",a[2][2],"|")
+    print("_______________")
+
+def run():
+    ws.Beep(1600, 200);
+    ws.Beep(1595, 400);
+    ws.Beep(1590, 600);
+    ws.Beep(1585, 800);
+    ws.Beep(1400, 600);
+    ws.Beep(1405, 400);
+    ws.Beep(1410, 200);
+    ws.Beep(1200, 1000);
+
+def check(a,ch):
+    c=0;
+    if(c!=1):
+        for i in range(0,3):
+            if(a[i][0]==ch and a[i][1]==ch and a[i][2]==ch):
+                c=1
+        if(c!=1):
+            for i in range(0,3):
+                if(a[0][i]==ch and a[1][i]==ch and a[2][i]==ch):
+                    c=1
+            if(c!=1):
+                if(a[0][0]==ch and a[1][1]==ch and a[2][2]==ch):
+                    c=1
+                if(c!=1):
+                    if(a[2][0]==ch and a[1][1]==ch and a[0][2]==ch):
+                        c=1
+    return c;
+
+def check_draw(a):
+    c=0
+    for i in range(0,3):
+        for j in range(0,3):
+            if(a[i][j]=='X' or a[i][j]=='O'):
+                c=c+1
+    return c;
+
+def position_filling(a,choice,ch):
+    if choice==1:
+        a[2][0] = ch
+    elif choice==2:
+        a[2][1] = ch
+    elif choice==3:
+        a[2][2] = ch
+    elif choice==4:
+        a[1][0] = ch
+    elif choice==5:
+        a[1][1] = ch
+    elif choice==6:
+        a[1][2] = ch
+    elif choice==7:
+        a[0][0] = ch
+    elif choice==8:
+        a[0][1] = ch
+    elif choice==9:
+        a[0][2] = ch
+    else:
+        print('INVALID CHOICE! Now your turn has skipped!')
+
+print('Choose your marking block by the given reference : ')
+print("7 | 8 | 9")
+print("----------")
+print("4 | 5 | 6")
+print("----------")
+print("1 | 2 | 3")
+a = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']] 
+p = input('Who Wants To Mark First (X or O) : ')
+if p=='X':
+    c = -1
+else:
+    c = 0
+m=0
+while(m!=1):
+    c = c+1
+    choice = 0
+    if(c%2==0):
+        while(choice<1 or choice>9):
+            choice = int(input('\nEnter the position of X according to the reference : '))
+        position_filling(a,choice,'X')
+        m = check(a,'X')
+        out(a)
+        if(m==1):
+            print('\nCongratulations...Player X Wins!!!\n')
+            run() 
+        if(check_draw(a)==9 and m!=1):
+            print("GAME IS DRAW")
+            m = 1
+    elif(c%2==1):
+        choice = 0
+        while(choice<1 or choice>9):
+            choice = int(input('\nEnter the position of O according to the reference : '))
+        position_filling(a,choice,'O')
+        m = check(a,'O')
+        out(a)
+        if(m==1):
+            print('\nCongratulations...Player O Wins!!!\n')
+            run() 
+        if(check_draw(a)==9 and m!=1):
+            print("GAME IS DRAW")
+            m = 1
